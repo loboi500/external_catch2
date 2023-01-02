d24a3d3c95ad4b53adc14435cc36f7a6f1831a60
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-03 01:32:10 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,3 @@
-#!/usr/bin/env python
-from conans import ConanFile, CMake
-
-
-class CatchConan(ConanFile):
-    name = "Catch2"
-    description = "A modern, C++-native, header-only, framework for unit-tests, TDD and BDD"
-    topics = ("conan", "catch2", "header-only", "unit-test", "tdd", "bdd")
-    url = "https://github.com/catchorg/Catch2"
-    homepage = url
-    license = "BSL-1.0"
-    exports = "LICENSE.txt"
-    exports_sources = ("single_include/*", "CMakeLists.txt", "CMake/*", "contrib/*")
-    generators = "cmake"
-
-    def package(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = "OFF"
-        cmake.definitions["CATCH_INSTALL_DOCS"] = "OFF"
-        cmake.definitions["CATCH_INSTALL_HELPERS"] = "ON"
-        cmake.configure(build_folder='build')
-        cmake.install()
-
-        self.copy(pattern="LICENSE.txt", dst="licenses")
-
-    def package_id(self):
-        self.info.header_only()
+version https://git-lfs.github.com/spec/v1
+oid sha256:5208924993b606cacb2db16b8bf5918d219814aeb462e2e0900ed1e515f957a5
+size 909
